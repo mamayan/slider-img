123ec050a621394cab12ef82443b373f96ad111a
马燕 <mosic.ma@hotmail.com>
2017-07-12 21:26:41 +0800

    layout

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by mayan on 2017/7/12.
+ */
+// (function () {
+//     var i=1;
+//     function clock(){
+//         i=i+1;
+//         if(i===5){
+//             i=1;
+//         }
+//     }
+//     function change(){
+//         var a=document.getElementById("transImageBox");
+//         a.style.marginLeft=(1-i)*400+"px";
+//     }
+//     setInterval('change()',2000);
+//     setInterval('clock()',3000);
+// })();
